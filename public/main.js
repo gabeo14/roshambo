cc20e293e387bb7d6d20fa6dc04d7a50f51deeb3
Gabe Oleinik <gomusic14@gmail.com>
2018-09-14 11:34:30 -0400

    english logic statements and button listeners

--- public/main.js
+++ public/main.js
@@ -1,3 +1,38 @@
-const main = () => {};
+let playerOneChoice = ""
+let playerTwoChoice = ""
 
-document.addEventListener("DOMContentLoaded", main);
+//IF player one choice is rock AND player two choice is scissors
+//OR player one choice is paper AND player two choice is rock
+//OR player one choice is scissors AND player two choice is paper
+//THEN player one wins
+
+//IF player two choice is rock AND player one choice is scissors
+//OR player two choice is paper AND player one choice is rock
+//OR player two choice is scissors AND player one choice is paper
+//THEN player two wins
+
+//IF player one choice is rock AND player two choice is rock
+//OR player one choice is paper AND player two choice is paper
+//OR player one choice is scissors AND player two choice is scissors
+//THEN tie
+const main = () => {
+    const playerOneRock = document.querySelector ('.player-1 .rock')
+    playerOneRock.addEventListener ('click', clickedPlayerOneRock)
+
+    const playerOnePaper = document.querySelector ('.player-1 .paper')
+    playerOnePaper.addEventListener ('click', clickedPlayerOnePaper)
+
+    const playerOneScissors = document.querySelector ('.player-1 .scissors')
+    playerOneScissors.addEventListener ('click', clickedPlayerOneScissors)
+
+    const playerTwoRock = document.querySelector ('.player-2 .rock')
+    playerTwoRock.addEventListener ('click', clickedPlayerTwoRock)
+
+    const playerTwoPaper = document.querySelector ('.player-2 .paper')
+    playerTwoPaper.addEventListener ('click', clickedPlayerTwoPaper)
+
+    const playerTwoScissors = document.querySelector ('.player-2 .scissors')
+    playerTwoScissors.addEventListener ('click' clickedPlayerTwoScissors)
+}
+
+document.addEventListener("DOMContentLoaded", main)
